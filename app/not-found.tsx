14e47dc7f17ef68a277209b5604758a9a31b06a5
0xPedroF <pedrobalo2@gmail.com>
2024-12-16 21:05:49 +0000

    Add 404 page with custom styling and animations

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main>
+      <div className="bg-indigo-900 relative overflow-hidden h-screen">
+        <img
+          src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9"
+          className="absolute h-full w-full object-cover"
+          alt="Background"
+        />
+        <div className="inset-0 bg-black opacity-25 absolute"></div>
+        <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+          <div className="w-full font-mono flex flex-col items-center relative z-10">
+            <h1 className="font-extrabold text-5xl text-center text-white leading-tight mt-4">
+              You are all alone here
+            </h1>
+            <p className="font-extrabold text-8xl my-44 text-white animate-bounce">
+              404
+            </p>
+            <a href="/" className="text-indigo-200 hover:underline">
+              <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                Go Back Home
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
